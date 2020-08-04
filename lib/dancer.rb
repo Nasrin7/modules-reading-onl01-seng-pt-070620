1b69175cd9add362f7d2a7653936f33477208bd0
Nasrin Rahimi <77.nasrin@gmail.com>
2020-08-04 11:12:41 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -5,10 +5,8 @@ require_relative './fancy_dance.rb'
 class Dancer
   # include Dance
   # extend MetaDancing
-  extend
-  FancyDance::ClassMethods
-  include
-  FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
   
   attr_reader :name
   
